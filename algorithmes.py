e9ae3dcadec7d22756f2997baf99313e7105e9f0
Alexis Dinanga <22303857@me.uvsq.fr>
2026-01-05 12:27:53 +0100

    Ajout de la fonction pondere iteratif au fichier algorithme

--- algorithmes.py
+++ algorithmes.py
@@ -12,15 +12,3 @@ def dfs_pondere_iteratif(graphe, source):
             for voisin, poids in reversed(graphe[sommet]):
                 if voisin not in visites:
                     pile.append((voisin, cout + poids))
-
-
-graphe = {
-    'A': [('B', 2), ('C', 5)],
-    'B': [('D', 4)],
-    'C': [('D', 1), ('E', 3)],
-    'D': [],
-    'E': []
-}
-
-
-dfs_pondere_iteratif(graphe, 'A')
