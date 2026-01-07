2dbc4b4d432ef4d47adb276565919fa4de8dbbf7
DINANGA Alexis <ADA37@dsone.3ds.com>
2026-01-07 11:41:41 +0100

    correction get_coueluer

--- affichage.py
+++ affichage.py
@@ -231,7 +231,7 @@ class Grillage:
 
             return int(max(abs(z1-z2),abs(w1-w2),abs(x1-x2)))
     def get_couleur(self,hexa):
-        return self.get_couleur(hexa)
+        return self.canvas.itemcget(hexa)
     
     def get_cout(self,hexa):
         couleur = self.get_couleur(hexa)
