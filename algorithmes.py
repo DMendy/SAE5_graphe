f3bc1e5afa886c50efc647b1eab75b8a1f62f42b
DINANGA Alexis <ADA37@dsone.3ds.com>
2026-01-07 22:55:44 +0100

    Suppression de méthodes obsolètes

--- algorithmes.py
+++ algorithmes.py
@@ -2,21 +2,6 @@ from collections import deque
 from time import sleep
 import random
 
-def dfs_iteratif(graphe, source):
-    visites = set()
-    pile = [(source, 0)]
-
-    while pile:
-        sommet, cout = pile.pop()
-
-        if sommet not in visites:
-            visites.add(sommet)
-            print(f"Visite {sommet} | Coût cumulé : {cout}")
-
-            for voisin, poids in reversed(graphe[sommet]):
-                if voisin not in visites:
-                    pile.append((voisin, cout + poids))
-
 def dfs(grillage):
     visites = []
     chemin_final=[]
@@ -84,34 +69,6 @@ def bfs(grillage):
     for sommet in chemin_final:
         grillage.canvas.itemconfig(sommet, fill="yellow")
 
-def dijkstra_iteratif(graphe, source, objectif):
-    distances = {sommet: float('inf') for sommet in graphe}
-    distances[source] = 0
-    precedents = {source: None}
-    non_visites = list(graphe.keys())
-    
-    while non_visites:
-        noeud_courant = min(non_visites, key=lambda node: distances[node])
-        print(f"Visite du sommet '{noeud_courant}' avec une distance cumulée de {distances[noeud_courant]}")
-
-        if noeud_courant == objectif:
-            print(f"Objectif '{objectif}' atteint!")
-            chemin = []
-            while noeud_courant is not None:
-                chemin.append(noeud_courant)
-                noeud_courant = precedents[noeud_courant]
-            return chemin[::-1]
-        
-        for voisin, distance in graphe[noeud_courant]:
-            nouvelle_distance = distances[noeud_courant] + distance
-            if nouvelle_distance < distances[voisin]:
-                distances[voisin] = nouvelle_distance
-                precedents[voisin] = noeud_courant
-        
-        non_visites.remove(noeud_courant)
-
-    print(f"Objectif '{objectif}' inaccessible.")
-    return None
 
 
 def _dijkstra_choisir(distances, a_traiter):
@@ -187,27 +144,6 @@ def dijkstra(grillage):
     return chemin
 
 
-def methode_gloutonne_iteratif(graphe, source, objectif, heuristique):
-    noeud_courant = source
-    chemin = [noeud_courant]
-    visites = set()
-
-    while noeud_courant != objectif:
-        visites.add(noeud_courant)
-        voisins_non_visites = [(voisin, distance) for voisin, distance in graphe[noeud_courant] if voisin not in visites]
-        
-        if not voisins_non_visites:
-            print(f"Aucun voisin non visité disponible depuis '{noeud_courant}'. L'algorithme échoue.")
-            return None
-        
-        prochain_noeud, _ = min(voisins_non_visites, key=lambda x: heuristique[x[0]])
-        print(f"Avance vers '{prochain_noeud}' avec une heuristique de {heuristique[prochain_noeud]}")
-        chemin.append(prochain_noeud)
-        noeud_courant = prochain_noeud
-    
-    print(f"Objectif '{objectif}' atteint!")
-    return chemin
-
 def glouton(grillage):
     visites=[]
     grillage.canvas.delete("fleche")
@@ -267,21 +203,6 @@ def a_star(graphe, source, objectif, heuristique):
                 precedents[voisin] = courant
                 open_set.add(voisin)
     return None
-
-def bfs_iteratif(graphe, source):
-    visites = set()
-    file = deque([source])
-
-    visites.add(source)
-
-    while file:
-        sommet = file.popleft()
-        print(f"Visite {sommet}")
-
-        for voisin, _ in graphe[sommet]:
-            if voisin not in visites:
-                visites.add(voisin)
-                file.append(voisin)
 
 
 def bellman_ford(graphe, source):
